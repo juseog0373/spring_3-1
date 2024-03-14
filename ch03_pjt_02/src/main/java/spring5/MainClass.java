e8382bbb6c61498299949d975dc15410431ddc3a
juseog0373 <juseog0373@naver.com>
2024-03-14 15:49:02 +0900

    update: xml, java 수정

--- ch03_pjt_02/src/main/java/spring5/MainClass.java
+++ ch03_pjt_02/src/main/java/spring5/MainClass.java
@@ -1,8 +1,17 @@
 package spring5;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
     public static void main(String[] args) {
-        TransportationWalk tw = new TransportationWalk();
+//        TransportationWalk tw = new TransportationWalk();
+//        tw.move();
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+        TransportationWalk tw = ctx.getBean("tw", TransportationWalk.class);
         tw.move();
+
+        ctx.close();
     }
 }
